ef5adc2ef00241741ebbdb33a95d78485bbc7f76
Eric Heiden <eric@eric-heiden.de>
2016-10-01 17:59:20 -0700

    Implemented basis for BeliefVoxel

---
+++ src/BeliefTreeNode.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <cmath>
+
+#include <octomap/OcTreeDataNode.h>
+
+#include "parameters.hpp"
+
+class Belief
+{
+public:
+    typedef std::array<Parameters::NumType, Parameters::numParticles> Particles;
+    static const Particles particles;
+
+    Belief();
+
+    Parameters::NumType mean() const;
+    Parameters::NumType variance() const;
+
+    bool isBeliefValid() const;
+
+private:
+    Particles pdf;
+    static Particles generateParticles();
+};
+
+typedef octomap::OcTreeDataNode<Belief> BeliefTreeNode;
---
+++ src/parameters.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+class Parameters {
+public:
+    static const unsigned int numSteps = 100;
+    static const unsigned int numParticles = 101;
+    typedef float NumType;
+};
